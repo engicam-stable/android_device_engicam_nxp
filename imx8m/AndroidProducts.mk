799fa100a307fd42e8b55d931b501ce72cb31dc2
Domenico Acri <domenico.acri@engicam.com>
2022-03-18 12:37:14 +0100

    imx8mm icore starterkit: customization started

--- imx8m/AndroidProducts.mk
+++ imx8m/AndroidProducts.mk
@@ -27,23 +27,11 @@
 # Copied from build/target/product/AndroidProducts.mk
 
 PRODUCT_MAKEFILES := \
-  $(LOCAL_DIR)/evk_8mq/evk_8mq.mk \
-  $(LOCAL_DIR)/evk_8mq/evk_8mq_drm.mk \
   $(LOCAL_DIR)/evk_8mm/evk_8mm.mk \
-  $(LOCAL_DIR)/evk_8mm/evk_8mm_drm.mk \
-  $(LOCAL_DIR)/evk_8mn/evk_8mn.mk \
   $(LOCAL_DIR)/evk_8mp/evk_8mp.mk
 
 COMMON_LUNCH_CHOICES := \
-  evk_8mq-user \
-  evk_8mq-userdebug \
-  evk_8mq_drm-user \
-  evk_8mq_drm-userdebug \
   evk_8mm-user \
   evk_8mm-userdebug \
-  evk_8mm_drm-user \
-  evk_8mm_drm-userdebug \
-  evk_8mn-user \
-  evk_8mn-userdebug \
   evk_8mp-user \
   evk_8mp-userdebug
