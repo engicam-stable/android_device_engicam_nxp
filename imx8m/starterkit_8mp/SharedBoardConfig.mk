d604fc90bc33b67d3ae0b8816002c20437ca59a6
Domenico Acri <domenico.acri@engicam.com>
2022-04-20 07:26:27 +0200

    imx8mp icore starterkit: changed cmasize and added kernel module for display lvds

--- imx8m/starterkit_8mp/SharedBoardConfig.mk
+++ imx8m/starterkit_8mp/SharedBoardConfig.mk
@@ -76,7 +76,8 @@ BOARD_VENDOR_KERNEL_MODULES += \
     $(KERNEL_OUT)/drivers/net/phy/realtek.ko \
     $(KERNEL_OUT)/drivers/ptp/ptp.ko \
     $(KERNEL_OUT)/drivers/pps/pps_core.ko \
-    $(KERNEL_OUT)/drivers/net/ethernet/freescale/fec.ko
+    $(KERNEL_OUT)/drivers/net/ethernet/freescale/fec.ko \
+    $(KERNEL_OUT)/drivers/gpu/drm/panel/panel-lvds.ko
 else
 BOARD_VENDOR_KERNEL_MODULES +=     \
     $(KERNEL_OUT)/drivers/input/touchscreen/synaptics_dsx/synaptics_dsx_i2c.ko
